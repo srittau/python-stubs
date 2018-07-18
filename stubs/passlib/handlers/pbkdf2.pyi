ba3a1dcb918c9d5c93af3351e68ac98f90a7b61e
Sebastian Rittau <srittau@rittau.biz>
2018-07-18 13:19:59 +0200

    passlib fixes
    * Move pbkdf2 classes and functions to passlib.handlers.pbkdf2
    * Mark mixins as abstract
    * Move some methods from _DynamicPbkdf2DigestHandler to Pbkdf2DigestHandler

--- stubs/passlib/handlers/pbkdf2.pyi
+++ stubs/passlib/handlers/pbkdf2.pyi
@@ -1,4 +1,4 @@
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Union, Type
 
 from ..utils.handlers import HasRounds, HasRawSalt, HasRawChecksum, GenericHandler
 
@@ -11,3 +11,20 @@ class Pbkdf2DigestHandler(HasRounds, HasRawSalt, HasRawChecksum, GenericHandler)
     min_rounds: int = ...
     max_rounds: int = ...
     rounds_cost: str = ...
+    @classmethod
+    def from_string(cls, hash: Union[str, bytes]) -> Pbkdf2DigestHandler: ...  # type: ignore
+    def to_string(self) -> bytes: ...  # type: ignore
+
+# dynamically created by create_pbkdf2_hash()
+class _DynamicPbkdf2DigestHandler(Pbkdf2DigestHandler):
+    name: str
+    ident: str
+    checksum_size: int
+    encoded_checksum_size: int
+
+def create_pbkdf2_hash(hash_name: str, digest_size: int, rounds: int = ..., ident: Optional[str] = ...,
+                       module: str = ...) -> Type[_DynamicPbkdf2DigestHandler]: ...
+
+pbkdf2_sha1: Type[_DynamicPbkdf2DigestHandler]
+pbkdf2_sha256: Type[_DynamicPbkdf2DigestHandler]
+pbkdf2_sha512: Type[_DynamicPbkdf2DigestHandler]
--- stubs/passlib/hash.pyi
+++ stubs/passlib/hash.pyi
@@ -1,21 +1,5 @@
-from typing import Optional, Type, Union
-
-from .handlers.pbkdf2 import Pbkdf2DigestHandler
-
-# dynamically created by create_pbkdf2_hash()
-class _DynamicPbkdf2DigestHandler(Pbkdf2DigestHandler):
-    name: str
-    ident: str
-    checksum_size: int
-    encoded_checksum_size: int
-
-    @classmethod
-    def from_string(cls, hash: Union[str, bytes]) -> _DynamicPbkdf2DigestHandler: ...  # type: ignore
-    def to_string(self) -> str: ...
-
-def create_pbkdf2_hash(hash_name: str, digest_size: int, rounds: int = ..., ident: Optional[str] = ...,
-                       module: str = ...) -> Type[_DynamicPbkdf2DigestHandler]: ...
-
-pbkdf2_sha1: Type[_DynamicPbkdf2DigestHandler]
-pbkdf2_sha256: Type[_DynamicPbkdf2DigestHandler]
-pbkdf2_sha512: Type[_DynamicPbkdf2DigestHandler]
+from .handlers.pbkdf2 import (
+    pbkdf2_sha1 as pbkdf2_sha1,
+    pbkdf2_sha256 as pbkdf2_sha256,
+    pbkdf2_sha512 as pbkdf2_sha512,
+)
