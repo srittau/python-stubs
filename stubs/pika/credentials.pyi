62504cfd573ef6546cc7c0701ca4beb31dcf5b56
Sebastian Rittau <srittau@rittau.biz>
2019-08-10 13:21:32 +0200

    Improve pika stubs

--- stubs/pika/credentials.pyi
+++ stubs/pika/credentials.pyi
@@ -1,2 +1,8 @@
-class PlainCredentials(object): ...
+class PlainCredentials(object):
+    TYPE: str
+    username: str
+    password: str
+    erase_on_connect: bool
+    def __init__(self, username: str, password: str, erase_on_connect: bool = ...) -> None: ...
+
 class ExternalCredentials(object): ...
