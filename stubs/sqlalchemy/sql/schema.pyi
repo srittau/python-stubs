170067000eb918ac32925b3c7e20fd2b4113cd1a
Sebastian Rittau <srittau@rittau.biz>
2019-12-04 20:51:13 +0100

    Derive some Column[...] generic types automatically

--- stubs/sqlalchemy/sql/schema.pyi
+++ stubs/sqlalchemy/sql/schema.pyi
@@ -1,14 +1,415 @@
-from typing import Any, Generic, TypeVar, Type, Union, Callable, overload, Sequence, Optional
+import datetime
+from typing import Any, Generic, TypeVar, Type, Union, Callable, overload, \
+    Sequence, Optional, Literal
 
 from ..ext.declarative.api import _DeclarativeBase
 from .base import DialectKWArgs, SchemaEventTarget
 from .elements import ColumnClause
 from .selectable import TableClause
+from .sqltypes import Integer, TIMESTAMP, Boolean, String, Unicode, JSON, Date, \
+    DateTime
 from .type_api import TypeEngine
 
 _T = TypeVar("_T")
 
 class Column(ColumnClause[_T], Generic[_T]):
+    @overload
+    def __init__(
+        self: Column[bool],
+        type_: Type[Boolean],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[False],
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[Optional[bool]],
+        type_: Type[Boolean],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[True] = ...,
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[bool],
+        column: str,
+        type_: Type[Boolean],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[False],
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[Optional[bool]],
+        column: str,
+        type_: Type[Boolean],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[True] = ...,
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+
+    @overload
+    def __init__(
+        self: Column[int],
+        type_: Type[Integer],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[False],
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[int],
+        type_: Type[Integer],
+        *args: Any,
+        primary_key: Literal[True],
+        nullable: bool = ...,
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[Optional[int]],
+        type_: Type[Integer],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[True] = ...,
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[int],
+        column: str,
+        type_: Type[Integer],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[False],
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[int],
+        column: str,
+        type_: Type[Integer],
+        *args: Any,
+        primary_key: Literal[True],
+        nullable: bool = ...,
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[Optional[int]],
+        column: str,
+        type_: Type[Integer],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[True] = ...,
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+
+    @overload
+    def __init__(
+        self: Column[str],
+        type_: Union[Type[String], String, Type[Unicode], Unicode],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[False],
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[str],
+        type_: Union[Type[String], String, Type[Unicode], Unicode],
+        *args: Any,
+        primary_key: Literal[True],
+        nullable: bool = ...,
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[Optional[str]],
+        type_: Union[Type[String], String, Type[Unicode], Unicode],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[True] = ...,
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[str],
+        column: str,
+        type_: Union[Type[String], String, Type[Unicode], Unicode],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[False],
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[str],
+        column: str,
+        type_: Union[Type[String], String, Type[Unicode], Unicode],
+        *args: Any,
+        primary_key: Literal[True],
+        nullable: bool = ...,
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[Optional[str]],
+        column: str,
+        type_: Union[Type[String], String, Type[Unicode], Unicode],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[True] = ...,
+        unique: bool = ...,
+        default: Union[_T, Callable[[], _T]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+
+    @overload
+    def __init__(
+        self: Column[datetime.date],
+        type_: Union[Date, Type[Date]],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[False],
+        unique: bool = ...,
+        default: Union[datetime.date, Callable[[], datetime.date]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[datetime.date],
+        type_: Union[Date, Type[Date]],
+        *args: Any,
+        primary_key: Literal[True],
+        nullable: bool = ...,
+        unique: bool = ...,
+        default: Union[datetime.date, Callable[[], datetime.date]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[Optional[datetime.date]],
+        type_: Union[Date, Type[Date]],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[True] = ...,
+        unique: bool = ...,
+        default: Union[Optional[datetime.date], Callable[[], Optional[datetime.date]]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[datetime.date],
+        column: str,
+        type_: Union[Date, Type[Date]],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[False],
+        unique: bool = ...,
+        default: Union[datetime.date, Callable[[], datetime.date]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[datetime.date],
+        column: str,
+        type_: Union[Date, Type[Date]],
+        *args: Any,
+        primary_key: Literal[True],
+        nullable: bool = ...,
+        unique: bool = ...,
+        default: Union[datetime.date, Callable[[], datetime.date]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[Optional[datetime.date]],
+        column: str,
+        type_: Union[Date, Type[Date]],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[True] = ...,
+        unique: bool = ...,
+        default: Union[Optional[datetime.date], Callable[[], Optional[datetime.date]]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+
+    @overload
+    def __init__(
+        self: Column[datetime.datetime],
+        type_: Union[TIMESTAMP, Type[TIMESTAMP], DateTime, Type[DateTime]],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[False],
+        unique: bool = ...,
+        default: Union[datetime.datetime, Callable[[], datetime.datetime]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[datetime.datetime],
+        type_: Union[TIMESTAMP, Type[TIMESTAMP], DateTime, Type[DateTime]],
+        *args: Any,
+        primary_key: Literal[True],
+        nullable: bool = ...,
+        unique: bool = ...,
+        default: Union[datetime.datetime, Callable[[], datetime.datetime]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[Optional[datetime.datetime]],
+        type_: Union[TIMESTAMP, Type[TIMESTAMP], DateTime, Type[DateTime]],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[True] = ...,
+        unique: bool = ...,
+        default: Union[Optional[datetime.datetime], Callable[[], Optional[datetime.datetime]]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[datetime.datetime],
+        column: str,
+        type_: Union[TIMESTAMP, Type[TIMESTAMP], DateTime, Type[DateTime]],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[False],
+        unique: bool = ...,
+        default: Union[datetime.datetime, Callable[[], datetime.datetime]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[datetime.datetime],
+        column: str,
+        type_: Union[TIMESTAMP, Type[TIMESTAMP], DateTime, Type[DateTime]],
+        *args: Any,
+        primary_key: Literal[True],
+        nullable: bool = ...,
+        unique: bool = ...,
+        default: Union[datetime.datetime, Callable[[], datetime.datetime]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[Optional[datetime.datetime]],
+        column: str,
+        type_: Union[TIMESTAMP, Type[TIMESTAMP], DateTime, Type[DateTime]],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[True] = ...,
+        unique: bool = ...,
+        default: Union[Optional[datetime.datetime], Callable[[], Optional[datetime.datetime]]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+
+    @overload
+    def __init__(
+        self: Column[Any],
+        type_: Union[JSON, Type[JSON]],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[False],
+        unique: bool = ...,
+        default: Union[Any, Callable[[], Any]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[Any],
+        type_: Union[JSON, Type[JSON]],
+        *args: Any,
+        primary_key: Literal[True],
+        nullable: bool = ...,
+        unique: bool = ...,
+        default: Union[Any, Callable[[], Any]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[Optional[Any]],
+        type_: Union[JSON, Type[JSON]],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[True] = ...,
+        unique: bool = ...,
+        default: Union[Optional[Any], Callable[[], Optional[Any]]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[Any],
+        column: str,
+        type_: Union[JSON, Type[JSON]],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[False],
+        unique: bool = ...,
+        default: Union[Any, Callable[[], Any]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[Any],
+        column: str,
+        type_: Union[JSON, Type[JSON]],
+        *args: Any,
+        primary_key: Literal[True],
+        nullable: bool = ...,
+        unique: bool = ...,
+        default: Union[Any, Callable[[], Any]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self: Column[Optional[Any]],
+        column: str,
+        type_: Union[JSON, Type[JSON]],
+        *args: Any,
+        primary_key: bool = ...,
+        nullable: Literal[True] = ...,
+        unique: bool = ...,
+        default: Union[Optional[Any], Callable[[], Optional[Any]]] = ...,
+        server_default: str = ...,
+    ) -> None: ...
+
     @overload
     def __init__(
         self,
@@ -31,6 +432,7 @@ class Column(ColumnClause[_T], Generic[_T]):
         unique: bool = ...,
         default: Union[_T, Callable[[], _T]] = ...,
         server_default: str = ...,
+
     ) -> None: ...
     @overload
     def __get__(self, instance: _DeclarativeBase, owner: Any) -> _T: ...
