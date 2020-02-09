89f249f30ab94af69d704f500fd463f29b6f0d84
Sebastian Rittau <srittau@rittau.biz>
2020-02-09 16:39:04 +0100

    Add jsonschema format checkers

--- stubs/jsonschema/__init__.pyi
+++ stubs/jsonschema/__init__.pyi
@@ -1,2 +1,11 @@
+# incomplete
+
+from ._format import (
+    FormatChecker as FormatChecker,
+    draft3_format_checker as draft3_format_checker,
+    draft4_format_checker as draft4_format_checker,
+    draft6_format_checker as draft6_format_checker,
+    draft7_format_checker as draft7_format_checker,
+)
 from .exceptions import ValidationError as ValidationError
 from .validators import validate as validate, RefResolver as RefResolver
---
+++ stubs/jsonschema/_format.pyi
@@ -0,0 +1,8 @@
+# incomplete
+
+class FormatChecker: ...  # incomplete
+
+draft3_format_checker: FormatChecker
+draft4_format_checker: FormatChecker
+draft6_format_checker: FormatChecker
+draft7_format_checker: FormatChecker
