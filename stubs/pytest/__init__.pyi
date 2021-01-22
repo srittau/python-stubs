d54a60315b260a06e50ed6365935b8a4a8bb8822
Sebastian Rittau <srittau@rittau.biz>
2021-01-22 18:52:31 +0100

    pytest: Fix import semantics

--- stubs/pytest/__init__.pyi
+++ stubs/pytest/__init__.pyi
@@ -1,4 +1,5 @@
 from _pytest.fixtures import fixture as fixture
-from _pytest.mark import MARK_GEN as mark
+from _pytest.mark import MARK_GEN
 from _pytest.outcomes import fail as fail
 from _pytest.python_api import raises as raises
+mark = MARK_GEN
