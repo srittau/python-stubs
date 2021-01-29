a1fe525842ea3f574ff198356538ade763ed7be6
Sebastian Rittau <srittau@rittau.biz>
2021-01-29 20:31:29 +0100

    Extend SQLalchemy

--- stubs/sqlalchemy/sql/__init__.pyi
+++ stubs/sqlalchemy/sql/__init__.pyi
@@ -1,10 +1,12 @@
 from .elements import (
+    ColumnElement as ColumnElement,
     not_ as not_,
 )
 from .expression import (
     and_ as and_,
     desc as desc,
     func as func,
+    insert as insert,
     or_ as or_,
     select as select,
 )
