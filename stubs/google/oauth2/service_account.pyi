e903411c9aa39ed8de3ba8a988494f0a170019c9
Sebastian Rittau <srittau@rittau.biz>
2021-09-29 15:43:11 +0200

    Add google.oauth2 stubs (incomplete)
    Accept oauth2 credentials for googleapiclient

---
+++ stubs/google/oauth2/service_account.pyi
@@ -0,0 +1,23 @@
+from collections.abc import Mapping, Sequence
+from typing import Any
+
+class Credentials:
+    def __init__(self,
+        signer: Any,
+        service_account_email: str,
+        token_uri: str,
+        scopes: Sequence[str] | None = ...,
+        default_scopes: Sequence[str] | None = ...,
+        subject: str | None = ...,
+        project_id: str | None = ...,
+        quota_project_id: str | None = ...,
+        additional_claims: Mapping[str, str] | None = ...,
+        always_use_jwt_access: bool | None = ...,
+) -> None: ...
+    @classmethod
+    def from_service_account_file(cls, filename: str, **kwargs: Any) -> Credentials: ...
+
+    def with_scopes(self, scopes: Sequence[str] | None, default_scopes: Sequence[str] | None = ...) -> Credentials: ...
+
+
+    # incomplete
--- stubs/googleapiclient/discovery.pyi
+++ stubs/googleapiclient/discovery.pyi
@@ -1,8 +1,9 @@
-from typing import Optional, Type, Any
+from typing import Type, Any
 
 from httplib2 import Http
 
-from oauth2client.client import Credentials
+from google.oauth2.service_account import Credentials
+from oauth2client.client import Credentials as OACredentials
 
 from .discovery_cache.base import Cache
 from .http import HttpRequest
@@ -11,14 +12,16 @@ from .model import Model
 class Resource(object):
     def __getattr__(self, item: str) -> Any: ...
 
-def build(serviceName: str,
-          version: str,
-          *,
-          http: Optional[Http] = ...,
-          discoveryServiceUrl: str = ...,
-          developerKey: Optional[str] = ...,
-          model: Optional[Model] = ...,
-          requestBuilder: Type[HttpRequest] = ...,
-          credentials: Optional[Credentials] = ...,
-          cache_discovery: bool = ...,
-          cache: Optional[Cache] = ...) -> Resource: ...
+def build(
+    serviceName: str,
+    version: str,
+    *,
+    http: Http | None = ...,
+    discoveryServiceUrl: str = ...,
+    developerKey: str | None = ...,
+    model: Model | None = ...,
+    requestBuilder: Type[HttpRequest] = ...,
+    credentials: Credentials | OACredentials | None = ...,
+    cache_discovery: bool = ...,
+    cache: Cache | None = ...,
+) -> Resource: ...
