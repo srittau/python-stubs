6f5a8d396418df7f9917edeeadf45247d0cdbcc2
Sebastian Rittau <srittau@rittau.biz>
2021-12-21 13:20:00 +0100

    Update SQLAlchemy declarative stubs

--- stubs/sqlalchemy/ext/declarative/__init__.pyi
+++ stubs/sqlalchemy/ext/declarative/__init__.pyi
@@ -1,4 +1,25 @@
-from .api import (
-    DeclarativeMeta as DeclarativeMeta,
-    declarative_base as declarative_base,
+from ...orm.decl_api import DeclarativeMeta as DeclarativeMeta, declared_attr as declared_attr
+from .extensions import (
+    AbstractConcreteBase as AbstractConcreteBase,
+    ConcreteBase as ConcreteBase,
+    DeferredReflection as DeferredReflection,
+    instrument_declarative as instrument_declarative,
 )
+
+__all__ = [
+    "declarative_base",
+    "synonym_for",
+    "has_inherited_table",
+    "instrument_declarative",
+    "declared_attr",
+    "as_declarative",
+    "ConcreteBase",
+    "AbstractConcreteBase",
+    "DeclarativeMeta",
+    "DeferredReflection",
+]
+
+def declarative_base(*arg, **kw): ...
+def as_declarative(*arg, **kw): ...
+def has_inherited_table(*arg, **kw): ...
+def synonym_for(*arg, **kw): ...
