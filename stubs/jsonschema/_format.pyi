656b712ebee4199a162b848c7f7cf0e1bcf031db
Sebastian Rittau <srittau@rittau.biz>
2022-01-25 10:36:11 +0100

    Remove jsonschema in favor of types-jsonschema

--- stubs/jsonschema/_format.pyi
+++
@@ -1,8 +0,0 @@
-# incomplete
-
-class FormatChecker: ...  # incomplete
-
-draft3_format_checker: FormatChecker
-draft4_format_checker: FormatChecker
-draft6_format_checker: FormatChecker
-draft7_format_checker: FormatChecker
